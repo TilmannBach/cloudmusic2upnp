6616eda971ae54cc43009beaf1c016a004b10d76
TilmannBach <tilli14@msn.com>
2013-01-07 22:11:38 +0100

    + added initial empty c# project and UPnP-lib

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenSource.UPnP;
+
+namespace cloudmusic2upnp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UPnPSmartControlPoint controlPoint = new UPnPSmartControlPoint();
+            controlPoint.OnAddedDevice += new UPnPSmartControlPoint.DeviceHandler(controlPoint_OnAddedDevice);
+            controlPoint.OnAddedService += new UPnPSmartControlPoint.ServiceHandler(controlPoint_OnAddedService);
+            controlPoint.Rescan();
+
+            Console.ReadLine();
+            return;
+        }
+
+        static void controlPoint_OnAddedService(UPnPSmartControlPoint sender, UPnPService service)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void controlPoint_OnAddedDevice(UPnPSmartControlPoint sender, UPnPDevice device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("cloudmusic2upnp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cloudmusic2upnp")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("1e01ea65-6ca1-4d34-9419-d09cb2cbfc42")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
