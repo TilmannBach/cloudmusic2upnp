c7343401107592b675e32a1af19aae208199348c
TilmannBach <tilli14@msn.com>
2013-01-08 18:15:22 +0100

    added Mono-Upnp lib and a small example of finding devices

--- Program.cs
+++ Program.cs
@@ -2,12 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using OpenSource.UPnP;
+// using OpenSource.UPnP;
+// using Mono.Upnp;
 
 namespace cloudmusic2upnp
 {
     class Program
     {
+        /// <summary>
+        /// Dictionary for all devices found. Hash is DeviceUdn.
+        /// </summary>
+        private Dictionary<string, Mono.Upnp.Device> myFoundDevices;
+
+        /// <summary>
+        /// Implementation test for Mono.Upnp
+        /// </summary>
+        /// <param name="args">none</param>
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+        }
+
+        Program()
+        {
+            myFoundDevices = new Dictionary<string, Mono.Upnp.Device>();
+
+            Mono.Upnp.Client controlPoint = new Mono.Upnp.Client();
+            controlPoint.DeviceAdded += new EventHandler<Mono.Upnp.DeviceEventArgs>(controlPoint_DeviceAdded);
+            controlPoint.ServiceAdded += new EventHandler<Mono.Upnp.ServiceEventArgs>(controlPoint_ServiceAdded);
+
+            controlPoint.BrowseAll();
+
+            Console.ReadLine();
+            return;
+        }
+
+        void controlPoint_ServiceAdded(object sender, Mono.Upnp.ServiceEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void controlPoint_DeviceAdded(object sender, Mono.Upnp.DeviceEventArgs e)
+        {
+            myFoundDevices.Add(e.Device.Udn, e.Device.GetDevice());
+            Console.WriteLine("Found new device! Its friendly name is: {0}", e.Device.GetDevice().FriendlyName);
+        }
+
+
+        /*
+         * 
+         * 
+         * 
+
+        /// <summary>
+        /// Implementation test for OpenSource.UPnP
+        /// </summary>
+        /// <param name="args">none</param>
         static void Main(string[] args)
         {
             UPnPSmartControlPoint controlPoint = new UPnPSmartControlPoint();
@@ -33,5 +83,7 @@ namespace cloudmusic2upnp
             }
             return;
         }
+         * 
+         */
     }
 }
