37c123a52fd3bcd5fa095676c0bf309af377bed0
Sven Walter <sven@sven-ngn.(none)>
2013-01-09 14:03:37 +0100

    Added a simple logging module.

--- Logger.cs
+++ Logger.cs
@@ -5,11 +5,24 @@ using System.Text;
 
 namespace cloudmusic2upnp
 {
-    public static class Logger
-    {
-        public static Logger(String message)
-        {
-            Console.WriteLine(message);
-        }
-    }
+	public class Logger
+	{
+		public enum Level
+		{
+			Debug,
+			Info,
+			Warning,
+			Error
+		}
+
+		public static void Log (String message)
+		{
+			Log (Level.Info, message);
+		}
+
+		public static void Log (Level level, String message)
+		{
+			Console.WriteLine("{0} [{1}] - {2}", DateTime.Now, level, message);
+		}
+	}
 }
