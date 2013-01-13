5a2b173536ddab9528df91e8351d57e2b25b905b
svenwltr <sven@wltr.eu>
2013-01-13 21:30:32 +0100

    Defined a simple interface for controlling devices.

---
+++ IDeviceController.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace cloudmusic2upnp
+{
+	/// <summary>
+	/// Interface for DeviceControllers, like a UPnP Control Point.
+	/// </summary>
+	public interface IDeviceController
+	{
+		/// <summary>
+		/// Gets a list with all avalible devices (eg UPnP Renderers).
+		/// </summary>
+		IDevice[] GetDevices();
+	}
+
+	public interface IDevice
+	{
+		/// <summary>
+		/// Play the currently selected media.
+		/// </summary>
+		void Play();
+
+		/// <summary>
+		/// Pause the currently playing media.
+		/// </summary>
+		void Pause();
+
+		/// <summary>
+		/// Stop the currently playing media.
+		/// </summary>
+		void Stop();
+
+		/// <summary>
+		/// Sets the media ressource to a specific URL.
+		/// </summary>
+		/// <param name='url'>
+		/// The URL, that should be played.
+		/// </param>
+		void SetMediaUrl(String url);
+	}
+}
+
