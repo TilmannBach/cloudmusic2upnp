713ed8bb91cc95f4c612750773a5f5623c9483b0
Sven Walter <sven@wltr.eu>
2013-01-18 16:29:39 +0100

    Fixed some bugs.

--- Core.cs
+++ Core.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace cloudmusic2upnp
 {
-    public class Core
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public Core()
-        {
-            Logger.Log("cloudmusic2upnp version " + 
-                System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " started");
+	public class Core
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public Core ()
+		{
+			Logger.Log ("cloudmusic2upnp version " + 
+				System.Reflection.Assembly.GetExecutingAssembly ().GetName ().Version.ToString () + " started"
+			);
 
-            DeviceController.IController UPnP = new DeviceController.UPnP();
+			DeviceController.IController UPnP = new DeviceController.UPnP ();
 
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine ();
+		}
+	}
 }
