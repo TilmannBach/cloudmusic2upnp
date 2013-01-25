9387087b617f7cfbf542756e6d9d01e49afe6c40
Sven Walter <sven@wltr.eu>
2013-01-25 15:47:41 +0100

    Improved HTTP server.

---
+++ UserInterface/Web/Http/WebServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using cloudmusic2upnp.Utils;
+
+namespace cloudmusic2upnp.Http
+{
+	public class WebServer
+	{
+		Listener runner;
+
+		public void Start ()
+		{
+			var cfg = Config.Load ();
+
+			runner = new Listener (ListenerCallback, "http://*:" + cfg.HttpPort + "/");
+			runner.Run ();
+		}
+
+		private static string ListenerCallback (System.Net.HttpListenerRequest request)
+		{
+			string[] urlPath = request.Url.AbsolutePath.Split ('/');
+			if (urlPath [urlPath.Length - 1] == String.Empty)
+				urlPath [urlPath.Length - 1] = "index.html";
+			string documentRoot = Path.Combine ("UserInterface", "Web", "Ressources");
+			string documentPath = Path.Combine (documentRoot, Path.Combine (urlPath));
+
+			try {
+				return System.IO.File.ReadAllText (documentPath, new System.Text.UTF8Encoding ());
+			} catch (FileNotFoundException) {
+				Logger.Log (Logger.Level.Warning, "HTTP 404 - File Not Found \"" + request.Url.AbsolutePath + "\"");
+				return "";
+			}
+
+
+		}
+
+		public void Stop ()
+		{
+			runner.Stop ();
+		}
+	}
+}
