7b87e273c87c3341014a61a2dbcca5f0a2a8c23e
Sven Walter <sven@wltr.eu>
2013-01-30 19:22:51 +0100

    First protocoll draft and minor stuff in WebSocket event handling.

--- UserInterface/Web/IWebManager.cs
+++ UserInterface/Web/IWebManager.cs
@@ -2,9 +2,21 @@ using System;
 
 namespace cloudmusic2upnp.UserInterface.Web
 {
-	public interface IWebManager
-	{
+    public class ConnectionOpenEventArgs : EventArgs
+    {
+        public IWebClient Client { get; private set; }
+
+        public ConnectionOpenEventArgs(IWebClient client)
+        {
+            Client = client;
+        }
+    }
+
+    public interface IWebManager
+    {
         void Stop();
-	}
+
+        event EventHandler<ConnectionOpenEventArgs> OnConnectionOpen;
+    }
 }
 
---
+++ UserInterface/Web/WebSocket/Client.cs
@@ -0,0 +1,13 @@
+using System;
+using Bauglir.Ex;
+
+namespace cloudmusic2upnp.UserInterface.Web.WebSocket
+{
+    public class Client : IWebClient
+    {
+        public Client(WebSocketConnection aConnection)
+        {
+        }
+    }
+}
+
