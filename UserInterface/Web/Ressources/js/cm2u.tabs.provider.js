1d3b6c33cd5210fdd670863840f74aa5cf2c3f7d
Tilmann Bach <tilli14@msn.com>
2014-02-04 15:54:34 +0100

    build a correct device and provider list

--- UserInterface/Web/Ressources/js/cm2u.tabs.provider.js
+++ UserInterface/Web/Ressources/js/cm2u.tabs.provider.js
@@ -2,17 +2,50 @@ cm2u.tabs.provider = (new function()
 {
     var module = {};
     
-    cm2u.event.register("ProviderNotification", "remote", function(event, data){
+    cm2u.event.register("ProviderNotification", "remote", function (event, data) {
         var tpl = $("#provider-template");
+
+        if (data.Providers.length == 0) {
+            tpl.nextAll().remove();
+        }
 
         for (var i = 0; i < data.Providers.length; i++) {
             var provider = data.Providers[i];
             var html = $(tpl.html());
             var label = html.find(".provider-label");
             var input = html.find(".provider-input");
-            
-            input.attr("id", "provider-"+provider.ID);
+
+            html.attr("id", "provider-" + provider.ID);
+            input.attr("id", "provider-" + provider.ID);
             label.text(provider.Name);
+
+            $("#tab-settings").find("#provider-" + provider.ID).remove();
+
+            tpl.after(html);
+        }
+
+    });
+
+    cm2u.event.register("DeviceNotification", "remote", function (event, data) {
+        var tpl = $("#renderers-template");
+
+        if (data.Devices.length == 0)
+        {
+            tpl.nextAll().remove();
+        }
+
+        for (var i = 0; i < data.Devices.length; i++) {
+            var device = data.Devices[i];
+            var html = $(tpl.html());
+            var label = html.find(".renderer-label");
+            var input = html.find(".renderer-input");
+
+            html.attr("id", "renderers-" + device.Udn);
+            input.attr("id", "renderer-" + device.Udn);
+            label.text(device.Name);
+            
+            $("#tab-settings").find("#renderers-" + device.Udn).remove();
+
             tpl.after(html);
         }
 
